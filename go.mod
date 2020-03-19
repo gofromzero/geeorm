447cbf4acaad86dbde363a4cb09e42a410870c19
沫雨 <1291953408@qq.com>
2020-03-19 12:40:09 +0800

    add log

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/gofromzero/geeorm
 
 go 1.14
+
+require (
+	github.com/astaxie/beego v1.12.1 // indirect
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible
+	github.com/shiena/ansicolor v0.0.0-20151119151921-a422bbe96644 // indirect
+	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
+)
---
+++ log/log.go
@@ -0,0 +1,60 @@
+// log
+// @Time : 2020/3/19 11:05 
+// @Author : Jeffery Sun
+// @File : log
+// @Software: GoLand
+
+package log
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"sync"
+)
+
+sqlite_master
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"sync"
+)
+
+var (
+	errorLog = log.New(os.Stdout, "\033[31m[error]]\033[0m", log.LstdFlags|log.Lshortfile)
+	infoLog = log.New(os.Stdout, "\033[34m[error]]\033[0m", log.LstdFlags|log.Lshortfile)
+	loggers = []*log.Logger{errorLog, infoLog}
+	mu sync.Mutex
+)
+
+var (
+	Error = errorLog.Println
+	Errorf = errorLog.Printf
+	Info = infoLog.Println
+	Infof = infoLog.Printf
+)
+// log levels
+const (
+	InfoLevel = iota
+	ErrorLevel
+	Disabled
+)
+
+// SetLevel controls log level
+func SetLevel(level int) {
+	mu.Lock()
+	defer mu.Unlock()
+	for _, logger := range loggers {
+		logger.SetOutput(os.Stdout)
+	}
+
+	if ErrorLevel < level {
+		errorLog.SetOutput(ioutil.Discard)
+	}
+
+	if InfoLevel < level {
+		infoLog.SetOutput(ioutil.Discard)
+	}
+
+}
